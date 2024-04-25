cbd9134c6fe629398e578d4adf423bf49ab5678d
Robert Nilsson <krnilsson@yandex.com>
2024-04-25 13:19:21 +0100

    Add validate data function to check input values

--- run.py
+++ run.py
@@ -2,4 +2,47 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-print("hello world")
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Lovesandwhichesdata')
+
+#sales = SHEET.worksheet('sales')
+
+def get_sales_data():
+    """
+    Get sales figures
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas")
+    print("Example: 10,20,30,40,50,60")
+
+    data_str = input("Enter your data here: ")
+    
+    sales_data = data_str.split(",")
+
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) !=6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+get_sales_data()
